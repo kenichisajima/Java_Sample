2881d2912b47525b8762c4a225047a521bfdf768
sajima_kenichi <sajicheer@gmail.com>
2022-06-18 19:57:22 +0900

    seller screen transition

--- JavaSampleProject/src/main/java/com/example/web/seller/SellerController.java
+++ JavaSampleProject/src/main/java/com/example/web/seller/SellerController.java
@@ -2,12 +2,69 @@ package com.example.web.seller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class SellerController {
 
+	// メニュー画面の購入者メニューボタンが押下された時の処理メソッドのフォワード後の処理メソッド
 	@RequestMapping(value = "/sellerPage")
 	public String sellerPage() {
 		return "seller/sellerPage";
 	}
+
+	// 販売者メニュー画面の商品登録ボタンが押下された時の処理メソッド
+	@RequestMapping(value = "/seller", params = "productRegister_btn", method = RequestMethod.POST)
+	public String toProductRegisterPage() {
+		return "seller/productRegister";
+	}
+
+	// 販売者メニュー画面の商品編集ボタンが押下された時の処理メソッド
+	@RequestMapping(value = "/seller", params = "productEdit_btn", method = RequestMethod.POST)
+	public String toProductEditPage() {
+		return "seller/productEdit";
+	}
+
+	// 販売者メニュー画面のメニュー選択に戻るボタンが押下された時の処理メソッド
+	@RequestMapping(value = "/seller", params = "backMenu_btn", method = RequestMethod.POST)
+	public String backMenuFromBuyerPage() {
+		return "user/menu";
+	}
+
+	// 商品登録画面の登録ボタンが押下された時の処理メソッド
+	@RequestMapping(value = "/productRegister", params = "register_btn", method = RequestMethod.POST)
+	public String productRegister() {
+		return "redirect:/productRegister?finish";
+	}
+
+	// 商品登録画面の登録ボタンが押下された時の処理メソッドのリダイレクト後の処理メソッド
+	@RequestMapping(value = "/productRegister", params = "finish", method = RequestMethod.GET)
+	public String redirectProductRegister() {
+		return "seller/productRegister";
+	}
+
+	// 商品登録画面の戻るボタンが押下された時の処理メソッド
+	@RequestMapping(value = "/productRegister", params = "back_btn", method = RequestMethod.POST)
+	public String backToSellerMenuFromProductRegister() {
+		return "seller/sellerPage";
+	}
+
+	// 商品編集画面の確定ボタンが押下された時の処理メソッド
+	@RequestMapping(value = "/productEdit", params = "fix_btn", method = RequestMethod.POST)
+	public String productEdit() {
+		return "redirect:/productEdit?finish";
+	}
+
+	// 商品編集画面の確定ボタンが押下された時の処理メソッドのリダイレクト後の処理メソッド
+	@RequestMapping(value = "/productEdit", params = "finish", method = RequestMethod.GET)
+	public String redirectProductEdit() {
+		return "seller/productEdit";
+	}
+
+	// 商品編集画面の戻るボタンが押下された時の処理メソッド
+	@RequestMapping(value = "/productEdit", params = "back_btn", method = RequestMethod.POST)
+	public String backToSellerMenuFromProductEdit() {
+		return "seller/sellerPage";
+	}
+
 }
